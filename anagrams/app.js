a668658b51278ba463715ed62e0da3a5da17252a
Mitch Congdon <mitchellcongdon@yahoo.com>
2017-06-16 10:32:35 -0400

    update
    bug fix

--- anagrams/app.js
+++ anagrams/app.js
@@ -8,11 +8,20 @@ function printPrevWords() {
     prevWords = [];
 };
 function save(){
+	//Select words
 	var word1 = document.getElementById("word1id").value;
 	var word2 = document.getElementById("word2id").value;
-	word1 = word1.split("").sort().join("");
-	word2 = word2.split("").sort().join("");
-	console.log(word1,word2);
+	/*takes words splits the letters up
+	sorts them alphabetically
+	rejoins them
+	removes white space
+	*/
+	word1 = word1.split("").sort().join("").replace(/ /g,"");
+	word2 = word2.split("").sort().join("").replace(/ /g,"");
+	//print words to console for trouble shooting
+	console.log("word 1 = " + word1);
+	console.log("word 2 = "+ word2);
+	//Resulting functions
 	if (word1 == word2){
 		prevWords.push(word1,word2);
 		console.log(prevWords);
